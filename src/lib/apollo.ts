cde2c0e4c44eb3939c87caba53c4ba59450375ae
Fhilipe Coelho <fhilipecoelho.dev@gmail.com>
2022-06-29 22:01:28 -0300

    feat: finished subscriber page

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4rlp4k7191001wgayad3rz3/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   // uri: 'http://localhost/graphql',
   cache: new InMemoryCache()
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        blur: 'url(/src/assets/blur-background.png)'
+      },
+      fontFamily: {
+        sans: 'Roboto, sans-serif',
+      },
       colors: {
-        fontFamily: {
-          sans: 'Roboto, sans-serif',
-        },
         green: {
           300: '#00B37E',
           500: '#00875F',
